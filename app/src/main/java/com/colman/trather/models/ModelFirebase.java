e51a9bb9c732b67e3d1d39c207e1c0671196fdd1
Nirel Didi <Nirel.didi@gmail.com>
2021-07-26 20:34:48 +0300

    Update SettingsRepo

--- app/src/main/java/com/colman/trather/models/ModelFirebase.java
+++ app/src/main/java/com/colman/trather/models/ModelFirebase.java
@@ -1,14 +1,21 @@
 package com.colman.trather.models;
 
 import android.content.Intent;
+import android.net.Uri;
 
 import com.colman.trather.Consts;
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -16,7 +23,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
+import static com.colman.trather.Consts.KEY_BIO;
+import static com.colman.trather.Consts.KEY_EMAIL;
+import static com.colman.trather.Consts.KEY_FULL_NAME;
+import static com.colman.trather.Consts.KEY_IMG_URL;
+
 public class ModelFirebase {
+
 
 
     public interface OnCompleteListener<T> {
@@ -70,4 +83,72 @@ public class ModelFirebase {
         return FirebaseAuth.getInstance().getCurrentUser() != null ? FirebaseAuth.getInstance().getCurrentUser().getUid() : null;
     }
     //endregion login
+
+
+    //region settings
+    public static void loadUser(OnCompleteListener<User> listener) {
+        CollectionReference usersCollection = FirebaseFirestore.getInstance().collection(Consts.USERS_COLLECTION);
+        usersCollection.document(Objects.requireNonNull(getUserUid())).get().addOnSuccessListener(documentSnapshot -> {
+            if (documentSnapshot.exists()) {
+                String imageUrl = documentSnapshot.getString(KEY_IMG_URL);
+                String fullName = documentSnapshot.getString(KEY_FULL_NAME);
+                String email = documentSnapshot.getString(KEY_EMAIL);
+                String bio = documentSnapshot.getString(KEY_BIO);
+                listener.onComplete(new User(getUserUid(), imageUrl, bio, fullName, email));
+            } else {
+                listener.onComplete(null);
+            }
+        });
+    }
+
+    public static void updateProfileImage(Uri uri, OnCompleteListener<String> listener) {
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
+        StorageReference riversRef = storageRef.child("images/" + uri.getLastPathSegment());
+        UploadTask uploadTask = riversRef.putFile(uri);
+
+        uploadTask.addOnFailureListener(exception -> {
+            listener.onComplete(null);
+        }).addOnSuccessListener(taskSnapshot -> {
+            final StorageMetadata metadata = taskSnapshot.getMetadata();
+            assert metadata != null;
+            final Task<Uri> downloadUri = Objects.requireNonNull(metadata.getReference()).getDownloadUrl();
+            downloadUri.addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    updateUserProfilePicture(downloadUri.getResult().toString(), listener);
+                } else
+                    listener.onComplete(null);
+            }).addOnFailureListener(e -> {
+                listener.onComplete(null);
+            });
+        });
+    }
+
+    public static void updateUserProfilePicture(String profileImageUrlNew, OnCompleteListener<String> listener) {
+        CollectionReference usersCollection = FirebaseFirestore.getInstance().collection(Consts.USERS_COLLECTION);
+        DocumentReference document = usersCollection.document(Objects.requireNonNull(getUserUid()));
+        Task<DocumentSnapshot> documentSnapshotTask = document.get();
+        documentSnapshotTask.addOnSuccessListener(queryDocumentSnapshots -> {
+            if (queryDocumentSnapshots != null) {
+                Map<String, Object> updates = new HashMap<>();
+                updates.put(Consts.KEY_IMG_URL, profileImageUrlNew);
+                document.update(updates);
+                listener.onComplete(profileImageUrlNew);
+            } else {
+                listener.onComplete(null);
+            }
+        });
+    }
+
+    public static void updateProfileData(String fullName, String bio, OnCompleteListener<Boolean> listener) {
+        CollectionReference usersCollection = FirebaseFirestore.getInstance().collection(Consts.USERS_COLLECTION);
+
+        DocumentReference document = usersCollection.document(getUserUid());
+        Map<String, Object> updates = new HashMap<>();
+        updates.put(KEY_FULL_NAME, fullName);
+        updates.put(KEY_BIO, bio);
+        document.update(updates).addOnCompleteListener(task -> listener.onComplete(task.isSuccessful()));
+    }
+
+    //endregion settings
 }
--- app/src/main/java/com/colman/trather/repositories/SettingsRepo.java
+++ app/src/main/java/com/colman/trather/repositories/SettingsRepo.java
@@ -1,60 +1,27 @@
 package com.colman.trather.repositories;
 
 import android.net.Uri;
-import android.text.TextUtils;
-import android.util.Log;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
-import com.colman.trather.Consts;
+import com.colman.trather.models.ModelFirebase;
 import com.colman.trather.models.User;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageMetadata;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 
-import static com.colman.trather.Consts.KEY_BIO;
-import static com.colman.trather.Consts.KEY_EMAIL;
-import static com.colman.trather.Consts.KEY_FULL_NAME;
-import static com.colman.trather.Consts.KEY_IMG_URL;
-
 public class SettingsRepo {
-    public static final String TAG = "SettingsRepository";
-    private final String userUid;
+
     private final MutableLiveData<User> user = new MutableLiveData<>(null);
     private final MutableLiveData<Boolean> isLoading = new MutableLiveData<>(false);
-    private final CollectionReference usersCollection;
 
     public SettingsRepo() {
-        userUid = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
-        usersCollection = FirebaseFirestore.getInstance().collection(Consts.USERS_COLLECTION);
     }
 
     public void loadUser() {
         isLoading.setValue(true);
-        usersCollection.document(userUid).get().addOnSuccessListener(documentSnapshot -> {
-            if (documentSnapshot.exists()) {
-                String imageUrl = documentSnapshot.getString(KEY_IMG_URL);
-                String fullName = documentSnapshot.getString(KEY_FULL_NAME);
-                String email = documentSnapshot.getString(KEY_EMAIL);
-                String bio = documentSnapshot.getString(KEY_BIO);
-                User user = new User(userUid, imageUrl, bio, fullName, email);
-                this.user.setValue(user);
-            } else {
-                Log.e(TAG, "Failed to get user data");
-            }
-
+        ModelFirebase.loadUser(user -> {
+            this.user.setValue(user);
             isLoading.setValue(false);
         });
     }
@@ -69,73 +36,23 @@ public class SettingsRepo {
 
     public void updateProfileImage(Uri uri) {
         isLoading.setValue(true);
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-        StorageReference storageRef = storage.getReference();
-        StorageReference riversRef = storageRef.child("images/" + uri.getLastPathSegment());
-        UploadTask uploadTask = riversRef.putFile(uri);
-
-        uploadTask.addOnFailureListener(exception -> {
-            isLoading.setValue(false);
-        }).addOnSuccessListener(taskSnapshot -> {
-            final StorageMetadata metadata = taskSnapshot.getMetadata();
-            assert metadata != null;
-            final Task<Uri> downloadUri = Objects.requireNonNull(metadata.getReference()).getDownloadUrl();
-            downloadUri.addOnCompleteListener(task -> {
-                if (task.isSuccessful()) {
-                    String profileImageUrlNew = downloadUri.getResult().toString();
-                    if (!TextUtils.isEmpty(profileImageUrlNew)) {
-                        updateUserProfilePicture(profileImageUrlNew);
-                    } else {
-                        isLoading.setValue(false);
-                        Log.e(TAG, "Failed to get new image uri");
-                    }
-                } else {
-                    isLoading.setValue(false);
-                    Log.e(TAG, "Failed to upload new image");
-                }
-            }).addOnFailureListener(e -> {
-                isLoading.setValue(false);
-                Log.e(TAG, "Failed to upload new image");
-            });
-        });
-    }
-
-    public void updateUserProfilePicture(String profileImageUrlNew) {
-        DocumentReference document = usersCollection.document(userUid);
-        Task<DocumentSnapshot> documentSnapshotTask = document.get();
-        documentSnapshotTask.addOnSuccessListener(queryDocumentSnapshots -> {
-            if (queryDocumentSnapshots != null) {
-                Map<String, Object> updates = new HashMap<>();
-                updates.put(Consts.KEY_IMG_URL, profileImageUrlNew);
-                document.update(updates);
-
+        ModelFirebase.updateProfileImage(uri, profileImageNewUrl -> {
+            if (profileImageNewUrl != null) {
                 User newUser = user.getValue();
-                Objects.requireNonNull(newUser).setImageUrl(profileImageUrlNew);
+                Objects.requireNonNull(newUser).setImageUrl(profileImageNewUrl);
                 user.setValue(newUser);
-            } else {
-                Log.e(TAG, "Failed to update user collection with new image url");
             }
-
             isLoading.setValue(false);
         });
     }
 
     public void updateProfileData(String fullName, String bio) {
-        DocumentReference document = usersCollection.document(userUid);
-        Task<DocumentSnapshot> documentSnapshotTask = document.get();
-        documentSnapshotTask.addOnSuccessListener(queryDocumentSnapshots -> {
-            if (queryDocumentSnapshots != null) {
-                Map<String, Object> updates = new HashMap<>();
-                updates.put(KEY_FULL_NAME, fullName);
-                updates.put(KEY_BIO, bio);
-                document.update(updates);
-
+        ModelFirebase.updateProfileData(fullName, bio, result -> {
+            if (result) {
                 User newUser = user.getValue();
                 Objects.requireNonNull(newUser).setFullname(fullName);
                 Objects.requireNonNull(newUser).setBio(bio);
                 user.setValue(newUser);
-            } else {
-                Log.e(TAG, "Failed to update user collection with new image url");
             }
 
             isLoading.setValue(false);
